f1f340ea323d2c3c70f2eba57f1791855b008037
Marisa Lia Pascal <87451265+Marisaliap@users.noreply.github.com>
2022-02-19 03:58:55 -0300

    changes for Amplify

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
+//  import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./App";
+// import { Provider } from "react-redux";
+// import store from "./store/store";
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   </Provider>,
+//   document.getElementById("root")
+// );
+// _____________________________________________________
+ 
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import GlobalStyle from "./styles/GlobalStyled";
+import Auth0ProviderWithHistory from './auth/auth0-provider-with-history';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <GlobalStyle />
+  <Router>
+    <Auth0ProviderWithHistory>
+      <App />
+    </Auth0ProviderWithHistory>
+  </Router>,
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
